16e23722ca05b5b842a180e2c596f185ca159501
BaiqingL <baiqinglyu@gmail.com>
2020-06-02 16:23:12 -0400

    Ping pong MVP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use serenity::{
+    model::{channel::Message, gateway::Ready},
+    prelude::*,
+};
+
+struct Handler;
+
+impl EventHandler for Handler {
+
+    fn message(&self, ctx: Context, msg: Message) {
+        if &msg.content[0..5] == "!rate" {
+            if let Err(reason) = msg.channel_id.say(&ctx.http, returnSentiment()) {
+                println!("Error sending message: {:?}", reason);
+            }
+        }
+    }
+
+    fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+fn returnSentiment() -> String {
+    String::from("It works")
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let token = "token";
+
+    // Create a new instance of the Client, logging in as a bot. This will
+    // automatically prepend your bot token with "Bot ", which is a requirement
+    // by Discord for bot users.
+    let mut client = Client::new(&token, Handler).expect("Err creating client");
+
+    // Finally, start a single shard, and start listening to events.
+    //
+    // Shards will automatically attempt to reconnect, and will perform
+    // exponential backoff until it reconnects.
+    if let Err(reason) = client.start() {
+        println!("Client error: {:?}", reason);
+    }
 }
